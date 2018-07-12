d965106d67c161a124184e2ed1f0460cbe93324f
Jhonatan <jzherran@gmail.com>
2018-07-12 16:27:25 -0500

    New challenges from hackerrank

---
+++ src/hackerrank/ElectronicShop.java
@@ -0,0 +1,36 @@
+package hackerrank;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class ElectronicShop {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int b = sc.nextInt();
+        Integer[] n = new Integer[sc.nextInt()];
+        int[] m = new int[sc.nextInt()];
+
+        for (int i = 0; i < n.length; i++) {
+            n[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < m.length; i++) {
+            m[i] = sc.nextInt();
+        }
+
+        Arrays.sort(n, Collections.reverseOrder());
+        Arrays.sort(m);
+
+        int max = -1;
+        for (int i = 0, j = 0; i < n.length; i++) {
+            for (; j < m.length; j++) {
+                if(n[i] + m[j] > b) break;
+                if(n[i] + m[j] > max) max = n[i] + m[j];
+            }
+        }
+
+        System.out.println(max);
+    }
+}
---
+++ src/hackerrank/FormingMagicSquare.java
@@ -0,0 +1,47 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+public class FormingMagicSquare {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int[][] s = new int[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                s[i][j] = in.nextInt();
+            }
+        }
+
+        System.out.println(formingMagicSquare(s));
+    }
+
+    static int formingMagicSquare(int[][] s) {
+
+        int[] totals = new int[6];
+
+        for (int i = 0, k = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                totals[k] += 1;
+                totals[k + 2] += 1;
+            }
+        }
+
+        return 0;
+    }
+}
+
+/**
+ 4   9   2 = 15
+ 3   5   7 = 15
+ 8   1   5 = 14
+ |   |   |
+ 15  15  14
+
+ 4   8   2 = 14
+ 4   5   7 = 16
+ 6   1   6 = 13
+ |   |   |
+ 14  14  15
+ */
