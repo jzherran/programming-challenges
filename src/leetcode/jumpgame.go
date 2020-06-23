e0c0bb41d67bbc190d6b7d5095833d19857352ee
Jhonatan Zambrano <jzherran@gmail.com>
2020-06-23 13:26:51 -0500

    Added 2 new solutions

--- src/leetcode/jumpgame.go
+++ src/leetcode/jumpgame.go
@@ -1,41 +1,68 @@
 package leetcode
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+
+	data, _ := reader.ReadString('\n')
+	nums := []int{}
+
+	for _, v := range strings.Split(data, ",") {
+
+		cv, e := strconv.Atoi(strings.ReplaceAll(v, "\n", ""))
+		if e == nil {
+			nums = append(nums, cv)
+		} else {
+			panic(e)
+		}
+	}
+
+	fmt.Println(canJump(nums))
+}
+
 // 55. Jump Game [https://leetcode.com/problems/jump-game/]
 // Greedy algorithm, first interaction with this concept.
 func canJump(nums []int) bool {
 	// init the max value in 0, because is not possible to get a negative values
-  maxValue := 0;
+	maxValue := 0
 
-  // iterate all values in nums array
-  for index, value := range nums {
+	// iterate all values in nums array
+	for index, value := range nums {
 
-    // if is not the first iteration we need to fix the max value stored
-    // that is necessary for the iteration of the nums array
-    if (index > 0) {
-      maxValue = maxValue - 1;
-    }
-    
-    // select the max value between the old max value and the new value in the array
-    maxValue = Max(maxValue, value);
-    
-    // logic to evaluate if is posible to jump to the last index in the nums array
-    if (len(nums) > 1 && maxValue == 0) {
-      return false;
-    } else if (len(nums) == 1 && maxValue == 1) {
-      return true;
-    } else if (maxValue >= ((len(nums) - 1) - index)) {
-      return true;
-    } else {
-      continue;
-    }
-  }
-  return false;
+		// if is not the first iteration we need to fix the max value stored
+		// that is necessary for the iteration of the nums array
+		if index > 0 {
+			maxValue = maxValue - 1
+		}
+
+		// select the max value between the old max value and the new value in the array
+		maxValue = Max(maxValue, value)
+
+		// logic to evaluate if is posible to jump to the last index in the nums array
+		if len(nums) > 1 && maxValue == 0 {
+			return false
+		} else if len(nums) == 1 && maxValue == 1 {
+			return true
+		} else if maxValue >= ((len(nums) - 1) - index) {
+			return true
+		} else {
+			continue
+		}
+	}
+	return false
 }
 
 // Max returns the larger of x or y.
 func Max(x, y int) int {
-    if x < y {
-        return y
-    }
-    return x
+	if x < y {
+		return y
+	}
+	return x
 }
---
+++ src/leetcode/longestPalindrome.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	reader := bufio.NewReader(os.Stdin)
+	s, _ := reader.ReadString('\n')
+
+	fmt.Println(longestPalindrome(s))
+}
+
+func print2DSlice(matrix [][]int) {
+	n := len(matrix)
+	m := len(matrix[0])
+	for i := 0; i < n; i++ {
+		for j := 0; j < m; j++ {
+			print(matrix[i][j], " ")
+		}
+		println()
+	}
+}
+
+/*
+
+First approach with brute force approximation.
+
+Time complex O(n^3)
+Space complex O(1)
+
+func longestPalindrome(s string) string {
+
+	str := strings.Split(strings.ReplaceAll(s, "\n", ""), "")
+	n := len(str)
+	maxLength, start := 1, 0
+
+	if n == 0 {
+		return ""
+	} else if n == 1 {
+		return s
+	} else {
+		for i := 0; i < n; i++ {
+			for j := i; j < n; j++ {
+
+				isPalindrome := true
+
+				for k := 0; k < (j-i+1)/2; k++ {
+					if str[i+k] != str[j-k] {
+						isPalindrome = false
+					}
+				}
+
+				if isPalindrome && (j-i+1) > maxLength {
+					start = i
+					maxLength = j - i + 1
+				}
+			}
+		}
+
+		var r strings.Builder
+
+		for i := start; i <= start+maxLength-1; i++ {
+			r.WriteString(str[i])
+		}
+
+		return r.String()
+  }
+}
+*/
+
+// 55. Jump Game [https://leetcode.com/problems/longest-palindromic-substring/]
+// Dynamic algorithm to calculate the longest palindrome substring
+func longestPalindrome(s string) string {
+
+	s = strings.ReplaceAll(s, "\n", "")
+	n := len(s)
+
+	if n == 0 {
+		return ""
+	} else if n == 1 {
+		return s
+	} else {
+		str := strings.Split(strings.ReplaceAll(s, "\n", ""), "")
+
+		t := make([][]int, n)
+		maxLength := 1
+
+		for i := 0; i < n; i++ {
+			t[i] = make([]int, n)
+			t[i][i] = 1
+		}
+
+		start := 0
+		for i := 0; i < n-1; i++ {
+			if str[i] == str[i+1] {
+				t[i][i+1] = 1
+				start = i
+				maxLength = 2
+			}
+		}
+
+		for k := 3; k <= n; k++ {
+			for i := 0; i < n-k+1; i++ {
+				j := i + k - 1
+				if t[i+1][j-1] == 1 && str[i] == str[j] {
+					t[i][j] = 1
+
+					if k > maxLength {
+						start = i
+						maxLength = k
+					}
+				}
+			}
+		}
+
+		//print2DSlice(t)
+
+		end := start + maxLength - 1
+
+		return s[start:end + 1]
+	}
+}
